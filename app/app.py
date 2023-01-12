377cf46c1395e6c9647db3d160c2e5a23e43e36f
lyta <solytateng2002@gmail.com>
2023-01-12 22:57:36 +0700

    test

--- app/app.py
+++ app/app.py
@@ -59,7 +59,10 @@ def home():
                 mst_path = mypack.saveGraph(MST[0], 'mst')
             
             for f in ['NN_0', 'NN_1', 'NN_2']:
-                NODES = len(list(G.nodes())) if NODES == None else NODES
+                try:
+                    NODES = len(list(G.nodes())) if NODES == None else NODES
+                except AttributeError:
+                    print(NODES)
                 A0, T0, W0, H0 = FUNC[f](G, NODES)
                 if len(H0) <= MAX_JRNY:
                     mypack.getJourneyFrames(G, H0)
